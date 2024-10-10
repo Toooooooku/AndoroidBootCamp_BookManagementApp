5451f887c9e8ca7cf9da96aaa7ab772d1b959c50
Rikuto <tokuriku1@gmail.com>
2024-10-10 22:43:20 +0900

    - Created ArticlesActivity, ArticlesViewModel, and related layouts - Updated BookRepository, BookManagementApp, and other existing files - Removed LegacyGetArticlesUseCase - Modified build configuration and project settings :q!

---
+++ app/src/main/java/com/example/androidbootcampiwatepref/ui/ArticlesActivity.kt
@@ -0,0 +1,4 @@
+package com.example.androidbootcampiwatepref.ui
+
+class ArticlesActivity {
+}
---
+++ app/src/main/java/com/example/androidbootcampiwatepref/viewmodel/ArticlesViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.androidbootcampiwatepref.viewmodel
+
+class ArticlesViewModel {
+}
